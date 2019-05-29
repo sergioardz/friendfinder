ad84d4f8edc8069de7454a5ede89c0ac55f9ac85
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-05-29 13:12:00 -0500

    Updated friends data and include some formatting for the modal, plus secure home display while using server on the htmlRoutes file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 var friends = [
     {
-        name: "Mr. No Face",
-        photo: "https://cdn12.picryl.com/photo/2016/12/31/head-the-dummy-avatar-people-c2eaed-1024.png",
+        name: "Daenerys Stormborn Targaryen",
+        photo: "https://i.pinimg.com/originals/4d/66/6c/4d666c8b610034eaa86658d0ea3cb06a.jpg",
         scores: [
-            "1",
-            "2",
-            "3",
             "2",
             "4",
-            "5",
+            "4",
+            "3",
+            "4",
+            "4",
             "5",
             "4",
             "3",
@@ -16,35 +16,115 @@ var friends = [
         ]
     },
     {
-        name: "Mr. Mannequin",
-        photo: "https://sc01.alicdn.com/kf/HTB1OOxaHVXXXXbwXpXXq6xXFXXXA/yellow-race-people-cloth-dummy.jpg_350x350.jpg",
+        name: "Jon Snow",
+        photo: "https://s3.r29static.com//bin/entry/4f0/720x864,85/1801540/image.webp",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "4",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Tyrion (The Imp) Lannister",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/44ccdd999a4b20cdaef4696b8bf1a2c502db2b66f6909ae2a8a96a01a698370688e9b73dad75f0d33ea170c58a251155-1-1558322295.jpg?crop=0.668xw:1.00xh;0.0918xw,0&resize=480:*",
         scores: [
             "5",
             "4",
+            "4",
+            "5",
+            "2",
+            "3",
             "3",
             "2",
-            "1",
-            "2",
-            "3",
-            "1",
             "4",
             "5"
         ]
     },
     {
-        name: "Mr. Crash TD",
-        photo: "https://assets-us-01.kc-usercontent.com/0542d611-b6d8-4320-a4f4-35ac5cbf43a6/7ec45d98-aae6-4bca-a76c-c8b366a43d17/Blog_wheels-wings-motors_are-crash-dummies-like-people_blog_header.jpg",
+        name: "Cersei Lannister",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Cersei_Lannister_in_Black_Dress_in_Season_5.jpg/220px-Cersei_Lannister_in_Black_Dress_in_Season_5.jpg",
         scores: [
             "4",
             "1",
-            "4",
+            "2",
             "1",
+            "1",
+            "3",
             "5",
+            "1",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Arya Stark",
+        photo: "https://s3.r29static.com//bin/entry/183/720x1009,85/2171313/stop-saying-arya-stark-isnt-2171313.webp",
+        scores: [
+            "3",
+            "4",
+            "3",
+            "5",
+            "4",
+            "5",
+            "3",
+            "5",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: "Sansa Stark",
+        photo: "https://i.pinimg.com/originals/11/1a/77/111a77f638e748c90c299d8d380552e4.jpg",
+        scores: [
+            "4",
+            "2",
+            "3",
+            "2",
+            "1",
+            "4",
+            "4",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Sandor (The Hound) Clegane",
+        photo: "https://i.pinimg.com/originals/9a/0e/6c/9a0e6cff14773a72157913dd0cfabf21.jpg",
+        scores: [
+            "1",
+            "2",
             "2",
             "3",
             "3",
+            "4",
             "5",
+            "3",
+            "3",
             "1"
+        ]
+    },
+    {
+        name: "Ser Jorah Mormont",
+        photo: "https://i.pinimg.com/originals/a6/2e/c6/a62ec675f2f1d20a7b7e68fc847ef5d9.jpg",
+        scores: [
+            "4",
+            "3",
+            "5",
+            "4",
+            "3",
+            "1",
+            "2",
+            "4",
+            "3",
+            "4"
         ]
     }
 ];
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,12 @@
 var path = require("path");
 
 module.exports = function(app) {
+    // Route to survey.html
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
-    app.get("/", function(req, res) {
+    // Route to home - By using "use" and not specifying a path, the default route will be then home
+    app.use(function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 };
