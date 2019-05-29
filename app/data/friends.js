8ce228baef6401d5696552bb5535f394459f9f42
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-05-28 21:25:56 -0500

    Finished Modal, app ready with basic functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Mr. No Face",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn12.picryl.com%2Fphoto%2F2016%2F12%2F31%2Fhead-the-dummy-avatar-people-c2eaed-1024.png&imgrefurl=https%3A%2F%2Fpicryl.com%2Fmedia%2Fhead-the-dummy-avatar-people-c2eaed&docid=Bi_d6fvDJxEGFM&tbnid=Xnp1YlKdVmou7M%3A&vet=10ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhEKAAwAA..i&w=1024&h=1024&bih=689&biw=1280&q=dummy%20pics%20people&ved=0ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhEKAAwAA&iact=mrc&uact=8",
+        photo: "https://cdn12.picryl.com/photo/2016/12/31/head-the-dummy-avatar-people-c2eaed-1024.png",
         scores: [
             "1",
             "2",
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         name: "Mr. Mannequin",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsc01.alicdn.com%2Fkf%2FHTB1OOxaHVXXXXbwXpXXq6xXFXXXA%2Fyellow-race-people-cloth-dummy.jpg_350x350.jpg&imgrefurl=https%3A%2F%2Fwww.alibaba.com%2Fproduct-detail%2Fyellow-race-people-cloth-dummy_60237113136.html&docid=uvt0_AiErY1qDM&tbnid=kcqmXs2BwZo6jM%3A&vet=10ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhMKAgwCA..i&w=350&h=350&bih=689&biw=1280&q=dummy%20pics%20people&ved=0ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhMKAgwCA&iact=mrc&uact=8",
+        photo: "https://sc01.alicdn.com/kf/HTB1OOxaHVXXXXbwXpXXq6xXFXXXA/yellow-race-people-cloth-dummy.jpg_350x350.jpg",
         scores: [
             "5",
             "4",
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         name: "Mr. Crash TD",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets-us-01.kc-usercontent.com%2F0542d611-b6d8-4320-a4f4-35ac5cbf43a6%2F7ec45d98-aae6-4bca-a76c-c8b366a43d17%2FBlog_wheels-wings-motors_are-crash-dummies-like-people_blog_header.jpg&imgrefurl=https%3A%2F%2Fwww.trustedchoice.com%2Finsurance-articles%2Fwheels-wings-motors%2Fcrash-test-dummies%2F&docid=4_03pqW8j0b_oM&tbnid=APqL7mWEqQ9GnM%3A&vet=10ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhSKA4wDg..i&w=750&h=281&bih=689&biw=1280&q=dummy%20pics%20people&ved=0ahUKEwjx44ig8r7iAhUCLKwKHanQCPgQMwhSKA4wDg&iact=mrc&uact=8",
+        photo: "https://assets-us-01.kc-usercontent.com/0542d611-b6d8-4320-a4f4-35ac5cbf43a6/7ec45d98-aae6-4bca-a76c-c8b366a43d17/Blog_wheels-wings-motors_are-crash-dummies-like-people_blog_header.jpg",
         scores: [
             "4",
             "1",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,38 @@ module.exports = function(app) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
+        // Create object to catch the best match
+        var match = {
+            name: "",
+            photo: "",
+            delta: 100
+        };
+
+        // Variables to retreive the form's info from survey.html and a keeper to compare deltas among friends object
         var newUser = req.body;
+        var newUserScores = newUser.scores;
+        var totalDelta = 0;
+
+        // for loop to go through friends object
+        for (i = 0; i < friends.length; i++) {
+
+            totalDelta = 0;
+
+            // Inner for loop to go through the scores array within each friend
+            for (j = 0; j < friends[i].scores.length; j++) {
+
+                totalDelta += Math.abs(parseInt(newUserScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDelta <= match.delta) {
+
+                    match.name = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.delta = totalDelta;
+                }
+            }
+        }
+
         friends.push(newUser);
-        res.json(newUser);
+        res.json(match);
     })
 };
